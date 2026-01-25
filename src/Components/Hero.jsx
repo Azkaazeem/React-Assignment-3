05855ae11f267fef6a687a3ff3a4f829246e6218
Azka Azeem <azkaazeem804@gmail.com>
2026-01-25 06:11:47 +0500

    name

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,14 +1,19 @@
 import '../Styling/Hero.css';
+import HeartLeaf from "../assets/11- animated_leaf.png";
 
 function Hero() {
     return (
         <>
-           {console.log("hi")}
+            {console.log("hi")}
 
-           <div className='small-aniated-logo'>
-                <h1><i>Marée</i></h1>
-           </div>
-           
+            <div className='hero-container'>
+                <div className='small-animated-logo'>
+                    <h1><i>Marée</i></h1>
+                    <img src={HeartLeaf} alt="" className='heart-leaf1'/>
+                    <img src={HeartLeaf} alt="" className='heart-leaf2'/>
+                </div>
+            </div>
+
         </>
     )
 }
