07113d3084a59f9361da025f293e58397d8cb9c8
Azka Azeem <azkaazeem804@gmail.com>
2026-01-25 05:52:02 +0500

    name

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+import './App.css';
+import Hero from './Components/Hero.jsx';
+
+
+function App() {
+
+  return (
+    <>
+      <Hero/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,16 @@
+import '../Styling/Hero.css';
+
+function Hero() {
+    return (
+        <>
+           {console.log("hi")}
+
+           <div className='small-aniated-logo'>
+                <h1><i>Marée</i></h1>
+           </div>
+           
+        </>
+    )
+}
+
+export default Hero;
